9f0577af1741f94dd0b867aa31c84f39b7cd1f29
pkminor <minorpk@gmail.com>
2021-07-31 08:03:53 +0300

    added readInput and readRegex

---
+++ Exercise01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercise01
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            String input = "\\d{1}";
+            Regex rx = new Regex(input, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            MatchCollection matches = rx.Matches("helllo15");
+
+            Console.WriteLine("Count "+matches.Count);
+
+            Console.Read();
+
+        }
+    }
+}
